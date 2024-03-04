7d1fc4beb76644e828f6841c322ac3bd2982e229
BakSnn <baskarev.nikita@mail.ru>
2024-03-04 10:50:58 +0300

    changed

--- src/main/java/ru/baksnn/project/JokeBot/Config/BotConfig.java
+++ src/main/java/ru/baksnn/project/JokeBot/Config/BotConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.ParseMode;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
